cf60761e603c3ce046879421c1cf99caf0f594e0
efatima <efatima@users.noreply.github.com>
2026-05-09 14:23:57 +0500

    Fix Turbopack workspace root

--- IssueTracker/next.config.js
+++ IssueTracker/next.config.js
@@ -2,7 +2,10 @@ import path from "node:path";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  outputFileTracingRoot: path.join(process.cwd()),
+  outputFileTracingRoot: path.join(process.cwd(), ".."),
+  turbopack: {
+    root: path.join(process.cwd(), "..")
+  },
   experimental: {
     serverActions: {
       bodySizeLimit: "10mb"
